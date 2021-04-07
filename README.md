8d6ae11280a0378ac0ffa42fbdd50a6573fb367d
Tim Bishop <tim@shac.org.nz>
2021-04-06 18:18:45 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
-# particle-generic-modbus
-Generic Modbus driver for particle.io Gen3 Argon or Boron IoT devices.
+# Generic Modbus RTU driver
+Generic Modbus driver firmware for RS-485 Featherwing with a Particle Argon or Boron microcontroller.
+
+Connect your Modbus RTU device to your phone using the Particle.io mobile app.
+
+A generic app that lets you set/get any modbus holding register.
+
+![RS-485 Featherwing](img/MRv0.8_ARGON.JPG)
+
+## Components
+
+* Modbus RTU device: default device id 1, 9600 baud rate, N81 - no partiy, 8 data bits, 1 stop bit.
+* RS-485 Featherwing ([github](https://github.com/timshac/RS-485-FeatherWing), [tindie](https://www.tindie.com/products/hudsonsonoma/rs-485-modbus-controller-with-mobile-app/))
+* Particle Argon or Boron ([particle.io](https://store.particle.io/collections/gen-3))
+
+## Connect hardware
+
+1. Wire the RS-485 Featherwing to the modbus RTU device.  Connect D1 <-> A and D2 <-> B
+2. Insert the Boron or Argon into the RS-485 Featherwing
+3. Power the RS-485 Featherwing with 5.2-36v DC (300mA @ 5.2vdc, or 150mA @ 24vdc), or via the usb port.
+4. Connnect particle Boron (Cellular) or Argon (WiFi) to the Particle cloud using the Particle mobile app: [Android](https://play.google.com/store/apps/details?id=io.particle.android.app), [iOS](https://apps.apple.com/us/app/particle-iot/id991459054)
+
+Then you are ready to flash the firmware
+
+## Flash Firmware
+
+Clone this repository, and install the [particle command line tools](https://docs.particle.io/tutorials/developer-tools/cli/)
+
+```
+# edit src/particle-generic-modbus.ino to set your device id, baud rate, and parity.
+# default device id is 1, baurd rate 9600, parity 8N1
+
+cd particle-generic-modbus
+# compile and flash over WiFi or Cellular
+particle flash device_name 
+
+# or, for usb connected particle Boron
+particle compile argon|boron --saveTo=out.bin
+particle serial list
+particle serial flash out.bin
+```
+
+## Monitor and Control your device
+### Particle Functions
+
+Open the Particle.io app on your phone to control your device. 
+
+Get the value of the first register (0) as unsigned integer
+```
+GetOrSet_Register_Unsigned_regORregEQvalue_0_65535
+0
+```
+
+Get the value of the first register (0) as signed integer
+
+```
+GetOrSet_Register_Signed_regORregEQvalue_-32767_32767
+0
+```
+
+Set the value of register 8 as unsigned integer
+```
+GetOrSet_Register_Unsigned_regORregEQvalue_0_65535
+8=25
+```
+
+Set the value of register 8 as signed integer
+```
+GetOrSet_Register_Unsigned_regORregEQvalue_0_65535
+8=-1
+```
+
+Each particle command posts a JSON event to report its operation. The event is a single enty array. The event is posted to *GenericModbus/cmd_resp* For example:
+```
+[
+  {
+    "function": "set_signed",
+    "command": "-625",
+    "result": "-625",
+    "error": 0
+  }
+]
+```
+
+
+### Particle Events
+
+Once a minute a status update is posted to the event *GenericModbus/status*
+```
+{
+  "0": 302,
+  "1": 616,
+  "2": 1024,
+  "3": 0
+}
+```
+
+Additional or specific registers can be included in the status report with a simple modification of the source code.
+
+## Let's talk - Office Hours
+
+Are you interested in this system of tools we are building? 
+
+We would love to hear about what you are working on!
+
+Please book a free 20 minute session - [Hudson Sonoma Office Hours](https://calendly.com/twbishop/hudson-sonoma-office-hours) - if you would like to talk about any of the following:
+
+* Is my equipment compatible with this RS-485 Featherwing for remote control?
+* Understanding what this is and why it might be useful for you
+* Customizing the firmware or mobile app
+* Feedback having used (or tried to use) this product
---
+++ src/particle-generic-modbus.ino
@@ -0,0 +1,299 @@
+	/*
+ * Project Generic Modbus Driver for Particle Gen3 Hardware (Argon wifi or Boron cellular)
+ * Description: modbus driver - get and set 16bit integer values (signed or unsigned)
+ * Author: tim@hudsonsonoma.com
+ * Date: 25 March 2021
+ */
+
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+
+#define NODE_UNIT_ID 1   // The address of the modbus unit to connect to
+#define NODE_BAUD 9600   // The baud rate of the modbus unit
+#define EVENT_NAME String("GenericModbus") // expect EVENT_NAME/status and EVENT_NAME/cmd_resp events to be published
+#define STATUS_FREQ 1 // minutes per status update (default every 1 minute)
+
+/*  
+1. Every STATUS_FREQ minutes, this status JSON will be published:
+EVENT_NAME/status:
+{
+  "0": 301,
+  "1": 403,
+  "2": 1024,
+  "3": 0
+}
+
+2. When a command is executed, this JSON will be published:
+
+GetOrSet_Register_Unsigned_regORregEQvalue_0_65535 OR
+GetOrSet_Register_Signed_regORregEQvalue_-32767_32767
+Enter modbus register number (0-indexed) to retrieve its value, as either a signed or unsigned 16-bit integer
+0
+(retrieves value of the first register)
+0=5
+(sets the value of the first register to 5)
+
+EVENT_NAME/cmd_resp:
+[
+  {
+    "function": "set_signed",
+    "command": "-625",
+    "result": "-625",
+    "error": 0
+  }
+]
+(typically a 1 element JSON array)
+
+3. You may send events that call functions to this device 
+Thanks to ParticleFunctionCaller(), you may also call Particle.functions from subscribed events:
+Subscribed by default to EVENT_NAME/function/{{{PARTICLE_DEVICE_ID}}} 
+
+for example:
+$ particle publish Novus322/function/e00fce68ee01234a8b1ffc0c '{"function":"set_setpoint_1","command":"29.9"}'
+
+You may direct webhook responses to this device (so that webhook response JSON may trigger particle functons)
+Add this configuration to the particle console: 
+Integrations / Your Integration / Edit / Advanced / Webhook Responses / Response Topic
+EVENT_NAME/function/{{{PARTICLE_DEVICE_ID}}}
+
+Functions may be called as such:  (set the first register (0) to the signed value -1)
+{"function":"set_signed","command":"0=-1"}
+
+*/
+
+
+
+#define CTRL_PIN D6
+#define TEMP_REGISTERS 0 // starting offset of modbus holding registers
+
+#include "ModbusMaster.h"
+#include "JsonParserGeneratorRK.h"
+
+SerialLogHandler logHandler(9600,LOG_LEVEL_WARN, {
+    {"app", LOG_LEVEL_TRACE},
+    {"system", LOG_LEVEL_INFO}
+});
+
+// connect to modbus node NODE_UNIT_ID
+ModbusMaster node(NODE_UNIT_ID);
+JsonWriterStatic<512> jw;
+
+void setup() {
+	pinMode(CTRL_PIN,OUTPUT);
+
+	//selectExternalMeshAntenna();  UNCOMMENT to select external antenna - ONLY IF IT IS CONNECTED.
+
+	node.begin(NODE_BAUD);
+	node.enableTXpin(CTRL_PIN);
+	node.enableDebug();
+
+    delay(5000);
+
+	// Edit or comment out to control what options are presented 
+	// in the Particle.io mobile app interface
+	// Also remember to add function calls to ParticleFunctionCaller()
+    Particle.function("GetOrSet_Register_Unsigned_regORregEQvalue_0_65535",set_unsigned);
+    Particle.function("GetOrSet_Register_Signed_regORregEQvalue_-32767_32767",set_signed);
+
+    // Handle incoming function calls via subscribe
+    // Uncomment the following line to allow webhooks to respond with particle function calls
+    // Particle.subscribe(EVENT_NAME + "/function/" + System.deviceID(), ParticleFunctionCaller, MY_DEVICES);
+
+    Particle.publishVitals(60*60); // publish vitals every hour 
+
+}
+
+void ParticleFunctionCaller(const char *eventName, const char *data) {
+
+	JSONValue outerObj = JSONValue::parseCopy(data);
+    JSONObjectIterator iter(outerObj);
+    String functionName;
+    String command;
+    bool hasFunction = false;
+    bool hasCommand = false;
+
+    while(iter.next()) {
+       	if ( String(iter.name()) == "function") { functionName = String(iter.value().toString()); hasFunction = true;}
+       	if ( String(iter.name()) == "command") { command = String(iter.value().toString()); hasCommand = true; }
+    }
+
+    // PUBLISHED FUNCTIONS: ADD ALL Particle Functions here that you wish to allow webhooks to call.
+    if (hasFunction && hasCommand) {
+    	if (functionName == "set_unsigned") { set_unsigned(command); }
+    	else if (functionName == "set_signed") { set_signed(command); }
+    	// .. add more as needed
+    } 
+
+}
+
+bool cmd_resp(String function, String command, String result, int error) {
+	jw.clear();
+	jw.startArray();
+	jw.startObject();
+	jw.insertKeyValue("function", function);
+	jw.insertKeyValue("command", command);
+	jw.insertKeyValue("result", result);
+	jw.insertKeyValue("error", error);
+	jw.finishObjectOrArray();
+	jw.finishObjectOrArray();
+	String json = jw.getBuffer();
+	jw.clear();
+	if (json.charAt(0) == ',') {
+		json.setCharAt(0,' ');
+	} // remove leading comma
+	return (Particle.publish(EVENT_NAME + "/cmd_resp", json.c_str(), PRIVATE));
+
+}
+
+/* ************************ */
+
+// Functions to set various modbus registers.
+// Code is identical, except for the macros which are custom to each register.
+int set_signed(String command) {
+#define REGISTER 0
+#define FUNCTION_NAME "set_signed"
+#define CONSTRAIN(floatval) (constrain(floatval, -32768, 32768))
+#define STRING_TO_MODBUS_VALUE(command,modbus_value) \
+	float floatval = command.toFloat();  \
+	floatval = CONSTRAIN(floatval);      \
+	modbus_value = (int16_t)(floatval)
+#define MODBUS_VALUE_TO_STRING(modbus_value) (String::format("%i", ((int16_t) modbus_value )) )
+
+	uint8_t modbus_code;
+	uint16_t modbus_value;
+	uint16_t value;
+
+	command = command.trim();
+	int i = command.indexOf('=');
+	if (i == -1 ) {
+		int offset = command.toInt();
+		modbus_code = node.readHoldingRegisters(REGISTER+offset,1);
+    	if (modbus_code == node.ku8MBSuccess) {
+            modbus_value = node.getResponseBuffer(0);
+            cmd_resp(FUNCTION_NAME,command,MODBUS_VALUE_TO_STRING(modbus_value),0);
+        	return (int16_t) modbus_value;
+		} else { cmd_resp(FUNCTION_NAME,command,"",-2); return -2; }
+	} else {	
+		int offset = command.substring(0,i-1).toInt();
+		String string_val = command.substring(i+1);
+		STRING_TO_MODBUS_VALUE(string_val,modbus_value);
+		modbus_code = node.writeSingleRegister(REGISTER,(uint16_t)modbus_value);
+		if (modbus_code != node.ku8MBSuccess) { cmd_resp(FUNCTION_NAME,command,"",-3); return -3; }
+		delay(250);
+		modbus_code = node.readHoldingRegisters(REGISTER,1);
+	    if (modbus_code == node.ku8MBSuccess) {
+	       	modbus_value = node.getResponseBuffer(0);
+	       	cmd_resp(FUNCTION_NAME,command,MODBUS_VALUE_TO_STRING(modbus_value),0);
+	       	return (int16_t) modbus_value;
+		} else { cmd_resp(FUNCTION_NAME,command,"",-4); return -4; }
+	}
+}
+
+int set_unsigned(String command) {
+#define REGISTER 0
+#define FUNCTION_NAME "set_unsigned"
+#define CONSTRAIN(floatval) (constrain(floatval, 0, 65535))
+#define STRING_TO_MODBUS_VALUE(command,modbus_value) \
+	float floatval = command.toFloat();  \
+	floatval = CONSTRAIN(floatval);      \
+	modbus_value = (uint16_t)(floatval)
+#define MODBUS_VALUE_TO_STRING(modbus_value) (String::format("%u", ((uint16_t) modbus_value )) )
+
+	uint8_t modbus_code;
+	uint16_t modbus_value;
+	uint16_t value;
+
+	command = command.trim();
+	int i = command.indexOf('=');
+	if (i == -1 ) {
+		int offset = command.toInt();
+		modbus_code = node.readHoldingRegisters(REGISTER+offset,1);
+    	if (modbus_code == node.ku8MBSuccess) {
+            modbus_value = node.getResponseBuffer(0);
+            cmd_resp(FUNCTION_NAME,command,MODBUS_VALUE_TO_STRING(modbus_value),0);
+        	return (int16_t) modbus_value;
+		} else { cmd_resp(FUNCTION_NAME,command,"",-2); return -2; }
+	} else {	
+		int offset = command.substring(0,i-1).toInt();
+		String string_val = command.substring(i+1);
+		STRING_TO_MODBUS_VALUE(string_val,modbus_value);
+		modbus_code = node.writeSingleRegister(REGISTER,(uint16_t)modbus_value);
+		if (modbus_code != node.ku8MBSuccess) { cmd_resp(FUNCTION_NAME,command,"",-3); return -3; }
+		delay(250);
+		modbus_code = node.readHoldingRegisters(REGISTER,1);
+	    if (modbus_code == node.ku8MBSuccess) {
+	       	modbus_value = node.getResponseBuffer(0);
+	       	cmd_resp(FUNCTION_NAME,command,MODBUS_VALUE_TO_STRING(modbus_value),0);
+	       	return (int16_t) modbus_value;
+		} else { cmd_resp(FUNCTION_NAME,command,"",-4); return -4; }
+	}
+}
+
+
+/* ************************* */
+
+unsigned long previousMillis1 = 0;
+void loop() {
+    // do nothing :P
+
+	unsigned long currentMillis1 = millis();
+    if(currentMillis1 - previousMillis1 > (STATUS_FREQ * 60 * 1000) ) {
+        previousMillis1 = currentMillis1;
+
+	    uint8_t result = node.readHoldingRegisters(TEMP_REGISTERS,4);
+
+	    if (result == node.ku8MBSuccess) {
+	        uint16_t value = node.getResponseBuffer(0);
+	        //Log.info("Received: %0x",value);
+
+				jw.startObject();
+
+				// Add various types of data
+				// Edit or comment out lines to control what
+				// data is reported in Particle.io mobile app Events
+				jw.insertKeyValue("0",  node.getResponseBuffer(0) );
+				jw.insertKeyValue("1", node.getResponseBuffer(1)  );
+				jw.insertKeyValue("2", node.getResponseBuffer(2));
+				jw.insertKeyValue("3", node.getResponseBuffer(3));
+				// Add more registers you wish to see in the status report.
+
+				jw.finishObjectOrArray();
+
+			Serial.printlnf("%s", jw.getBuffer());
+			String json = jw.getBuffer();
+			if (json.charAt(0) == ',') {
+				json.setCharAt(0,' ');
+			} // remove leading comma if neccesary
+			Particle.publish(EVENT_NAME + "/status", json.c_str(), PRIVATE); 
+			jw.clear();
+
+	    } else {
+	        Serial.print("Failed, Response Code: ");
+			Serial.print(result, HEX); 
+			Serial.println("");
+	    }
+
+	}
+
+	delay(250);
+	
+}
+
+
+void selectExternalMeshAntenna()
+{
+#if   (PLATFORM_ID == PLATFORM_ARGON)
+	digitalWrite(ANTSW1, 1);
+	digitalWrite(ANTSW2, 0);
+#elif (PLATFORM_ID == PLATFORM_BORON)
+	digitalWrite(ANTSW1, 0);
+#elif (PLATFORM_ID == PLATFORM_XENON)
+	digitalWrite(ANTSW1, 0);
+	digitalWrite(ANTSW2, 1);
+#endif
+}
+
+
+
+
+
